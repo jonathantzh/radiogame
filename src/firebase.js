d6fb0ef5d5fd528ef9438e4385d21ef10af7b5ed
jonathantzh <jonzhtan@gmail.com>
2024-12-10 07:27:26 +0800

    remove unused

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
